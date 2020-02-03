bb8d6aef21c02d591ddc2fde52c98c5966d86278
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2020-02-02 17:15:15 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+const express = require('express');
+const app = new express();
+
+const { accounts, users, writeJSON } = require('./data');
+
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+app.get('/profile', (req, res) => {
+    res.render('profile', {user: users[0] });
+});
+
+app.listen(3000, () => console.log('Project running!'));
